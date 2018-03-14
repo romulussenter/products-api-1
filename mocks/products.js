da6a125ddd35a24246c054a988192de6b9e9f085
Justin Dennison <justin1dennison@gmail.com>
2018-03-14 19:29:58 -0400

    created mock product data and served that as get request response

---
+++ mocks/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        _id: 'hjkjhgsajkdfh',
+        name: "Javelin (caution)",
+        price: 500,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    },
+    {
+        _id: 'alkskjdfklhasdslkjh',
+        name: 'Snail',
+        price: 3.99,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    },
+    {
+        _id: 'ajshdfdjhasdkjfhasdie',
+        name: 'Rug',
+        price: 96.75,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date()
+    }
+];
+
+
+
+
+module.exports = products;
