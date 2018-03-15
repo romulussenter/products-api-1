9a759f52958e275be8754069e436fbfcfcd2b220
Justin Dennison <justin1dennison@gmail.com>
2018-03-14 20:41:51 -0400

    implemented post route for products

--- routers/products.js
+++ routers/products.js
@@ -36,5 +36,22 @@ router.get('/products/:id', (req, res) => {
     });
 });
 
+//post means create
+router.post('/products', (req, res) => {
+    const productObject = productArrToObj(mockProducts);
+    const id = 100000000 * Math.random();
+    const newProduct = {
+        _id: id,
+        name: 'something new',
+        price: 1000,
+        created: new Date(),
+        imgSrc: 'https://via.placeholder.com/250x250'
+    };
+    mockProducts.push(newProduct);
+    res.status(201).json({
+        msg: 'successfully created product'
+    })
+});
+
 module.exports = router; //like export default
 
