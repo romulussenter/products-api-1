f855dd7d1acb4e2d96dab4e6df2e40f9afedf08b
Justin Dennison <justin1dennison@gmail.com>
2018-03-15 20:07:24 -0400

    created the product model

---
+++ models/product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    //name: String, //I would add more info about this property
+    name: {
+        required: true,
+        type: String,
+    },
+    imgSrc: String,
+    price: Number,
+    created: {
+        required: true,
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
